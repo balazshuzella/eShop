b418fd13bbac731a4fa87032aa84a4daf71df365
balazshuzella <huzella@agendaro.de>
2025-06-30 14:31:39 +0000

    Alle weiteren Änderungen am Repository eingetragen

--- src/Basket.API/Model/BasketItem.cs
+++ src/Basket.API/Model/BasketItem.cs
@@ -10,6 +10,16 @@ public class BasketItem : IValidatableObject
     public int Quantity { get; set; }
     public string PictureUrl { get; set; }
 
+    /// <summary>
+    /// Überprüft die Eigenschaften des <see cref="BasketItem"/>-Objekts auf Gültigkeit.
+    /// Stellt sicher, dass die Anzahl (<c>Quantity</c>) mindestens 1 ist.
+    /// Gibt eine Liste von <see cref="ValidationResult"/> zurück, die alle Validierungsfehler enthält.
+    /// </summary>
+    /// <param name="validationContext">Der Kontext, in dem die Validierung ausgeführt wird.</param>
+    /// <returns>
+    /// Eine Auflistung von <see cref="ValidationResult"/>, die alle gefundenen Validierungsfehler enthält.
+    /// Ist die Liste leer, sind alle geprüften Eigenschaften gültig.
+    /// </returns>
     public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
     {
         var results = new List<ValidationResult>();
